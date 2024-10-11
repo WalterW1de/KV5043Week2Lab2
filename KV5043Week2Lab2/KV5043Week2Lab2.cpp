29be6b2778ef140a277cbdc9d6ce6063c25a54b7
ianem <w23056266@northumbria.ac.uk>
2024-10-11 11:42:53 +0100

    using std::cin with C-style strings

--- KV5043Week2Lab2/KV5043Week2Lab2.cpp
+++ KV5043Week2Lab2/KV5043Week2Lab2.cpp
@@ -2,25 +2,10 @@
 
 int main()
 {
-	int age;
-	int day, month, year;
-
-	std::cout << "Enter your age in years: ";
-	std::cin >> age;
-	if (std::cin.fail())
-	{
-
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-		std::cout << "Input error:" << std::endl;
-		std::cout << "Enter a number: ";
-		std::cin >> age;
-	}
-		std::cout << "Enter your birthdate (day, month, year): ";
-		std::cin >> day >> month >> year;
-
-		std::cout << "You are: " << age << " years old." << std::endl;
-		std::cout << "You were born on " << day << "/" << month << "/" << year << std::endl;
+	char name[20];
+	std::cout << "Enter your name: ";
+	std::cin >> name;
+	std::cout << "Hello, " << name << "!" << std::endl;
 
 		return 0;
 }
