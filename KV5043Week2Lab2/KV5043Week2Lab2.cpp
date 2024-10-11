3c2c8a03e6896c6569ee8f54653a1719a2f6fb33
ianem <w23056266@northumbria.ac.uk>
2024-10-11 12:07:41 +0100

    Catching a buffer overflow with an if condition

--- KV5043Week2Lab2/KV5043Week2Lab2.cpp
+++ KV5043Week2Lab2/KV5043Week2Lab2.cpp
@@ -2,9 +2,16 @@
 
 int main()
 {
-	char name[5];
+	char name[20];
 	std::cout << "Enter your name: ";
 	std::cin.getline(name, 20);
+	if (std::cin.fail())
+	{
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+		std::cout << "Input error:" << std::endl;
+		std::cin.getline(name, 20);
+	}
 	std::cout << "Hello, " << name << "!" << std::endl;
 
 		return 0;
