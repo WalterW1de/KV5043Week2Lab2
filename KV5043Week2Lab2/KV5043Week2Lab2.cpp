a0dcca0ed6c19bb3ec2f1290a62486e8408947a5
ianem <w23056266@northumbria.ac.uk>
2024-10-11 12:09:35 +0100

    Catching a buffer overflow with a while loop

--- KV5043Week2Lab2/KV5043Week2Lab2.cpp
+++ KV5043Week2Lab2/KV5043Week2Lab2.cpp
@@ -5,7 +5,7 @@ int main()
 	char name[20];
 	std::cout << "Enter your name: ";
 	std::cin.getline(name, 20);
-	if (std::cin.fail())
+	while (std::cin.fail())
 	{
 		std::cin.clear();
 		std::cin.ignore(10000, '\n');
