fac9eb86885906f42d2d2daea1e6f6bdd942cb17
ianem <w23056266@northumbria.ac.uk>
2024-10-11 11:30:42 +0100

    Using a while loop with cin.fail(…), cin.clear(…) and cin.ignore(…)

--- KV5043Week2Lab2/KV5043Week2Lab2.cpp
+++ KV5043Week2Lab2/KV5043Week2Lab2.cpp
@@ -7,12 +7,20 @@ int main()
 
 	std::cout << "Enter your age in years: ";
 	std::cin >> age;
+	if (std::cin.fail())
+	{
 
-	std::cout << "Enter your birthdate (day, month, year): ";
-	std::cin >> day >> month >> year; 
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+		std::cout << "Input error:" << std::endl;
+		std::cout << "Enter a number: ";
+		std::cin >> age;
+	}
+		std::cout << "Enter your birthdate (day, month, year): ";
+		std::cin >> day >> month >> year;
 
-	std::cout << "You are: " << age << " years old." << std::endl;
-	std::cout << "You were born on " << day << "/" << month << "/" << year << std::endl;
+		std::cout << "You are: " << age << " years old." << std::endl;
+		std::cout << "You were born on " << day << "/" << month << "/" << year << std::endl;
 
 		return 0;
 }
